389e6d32306752514f4a180c248b14744a9085eb
Aaron <aaron@bettercoder.net>
2015-02-16 14:38:56 -0800

    Remove comment.

--- crypt.go
+++ crypt.go
@@ -14,6 +14,10 @@ const (
 
 var cryptTable []uint32
 
+func init() {
+	initCrypto()
+}
+
 func initCrypto() {
 	if cryptTable != nil {
 		return
@@ -43,8 +47,6 @@ type decryptReader struct {
 }
 
 func newDecryptReader(reader io.Reader, key1 uint32) *decryptReader {
-	initCrypto()
-
 	return &decryptReader{
 		reader: reader,
 		key1:   key1,
@@ -71,8 +73,6 @@ func (d *decryptReader) Read(buf []byte) (n int, err error) {
 }
 
 func decryptBlock(block []byte, length int, key1 uint32) {
-	initCrypto()
-
 	var value uint32
 	var key2 uint32 = 0xEEEEEEEE
 
@@ -89,45 +89,3 @@ func decryptBlock(block []byte, length int, key1 uint32) {
 		key2 = value + key2 + (key2 << 5) + 3
 	}
 }
-
-/*
-void prepareCryptTable()
-{
-    unsigned long seed = 0x00100001, index1 = 0, index2 = 0, i;
-
-    for(index1 = 0; index1 < 0x100; index1++)
-    {
-        for(index2 = index1, i = 0; i < 5; i++, index2 += 0x100)
-        {
-            unsigned long temp1, temp2;
-
-            seed = (seed * 125 + 3) % 0x2AAAAB;
-            temp1 = (seed & 0xFFFF) << 0x10;
-
-            seed = (seed * 125 + 3) % 0x2AAAAB;
-            temp2 = (seed & 0xFFFF);
-
-            cryptTable[index2] = (temp1 | temp2);
-        }
-    }
-}
-
-void DecryptBlock(void *block, long length, unsigned long key)
-{
-    unsigned long seed = 0xEEEEEEEE, unsigned long ch;
-    unsigned long *castBlock = (unsigned long *)block;
-
-    // Round to longs
-    length >>= 2;
-
-    while(length-- > 0)
-    {
-        seed += stormBuffer[0x400 + (key & 0xFF)];
-        ch = *castBlock ^ (key + seed);
-
-        key = ((~key << 0x15) + 0x11111111) | (key >> 0x0B);
-        seed = ch + seed + (seed << 5) + 3;
-        *castBlock++ = ch;
-    }
-}
-*/
